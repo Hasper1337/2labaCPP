14a32faa0ec614618fe216000b54e31e3c6f7b8f
Viner <Viner@HASPER>
2025-11-15 12:26:01 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApplication2/2lab.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class RGBColor {
+private:
+    int red;
+    int green;
+    int blue;
+
+    static int colorCount;
+public:
+
+    RGBColor() : red(0), green(0), blue(0) { // Конструктор по умолчанию черный цвет(0, 0, 0)
+        colorCount++;
+    }
+
+    RGBColor(int r, int g, int b) : red(r), green(g), blue(b) { // Конструктор с параметрами
+        colorCount++;
+    }
+
+    RGBColor(int r, int g) : red(r), green(g), blue(255) { // Конструктор с параметрами(r, g, 250)
+        colorCount++;
+    }
+
+    void Print(){
+        cout << "Создан цвет со следующими параметрами r = " << red << "\t g = " << green << "\t b = " << blue << endl;
+    }
+
+    int SetR(int valueR) {
+        red = valueR;
+    }
+
+    int SetG(int valueG) {
+        green = valueG;
+    }
+
+    int SetB(int valueB) {
+        blue = valueB;
+    }
+
+    // перегрузки //
+
+
+};
+
+
+
+int main()
+{
+    RGBColor a;
+    a.Print();
+}
